610cab726517c44a56c66afe34c500f71ff1d98d
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2023-12-19 14:43:58 -0700

    Update create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+    
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
+
 end
